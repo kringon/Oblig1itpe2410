6894fcff348917dd382dac19c466307e721f3fdf
zlokex <andre.hovda@gmail.com>
2016-03-03 05:59:24 +0100

    Server thread working smoothly. TODO: ServerSockets does not close on ServerThread shutdown. Pass them in as a parameter on startup for easy access maybe?

--- src/main/java/org/hioa/itpe/ServerThread.java
+++ src/main/java/org/hioa/itpe/ServerThread.java
@@ -22,7 +22,7 @@ public class ServerThread extends Thread {
 
 	private PrintWriter out;
 
-	private volatile boolean running;
+	//private volatile boolean running;
 
 	public ServerThread(Socket socket) {
 
@@ -30,13 +30,12 @@ public class ServerThread extends Thread {
 		this.socket = socket;
 		connectedClientId = -1;
 		protocol = new Protocol();
-		running = true;
+		//running = true;
 
 		// lastPrintedAction = -1;
 	}
 
 	public void run() {
-		outerwhile:
 		while (!Thread.currentThread().isInterrupted()) {
 
 			try {
@@ -55,11 +54,9 @@ public class ServerThread extends Thread {
 					if (output != null) {
 					
 						if (output.getMessageType() == Message.ACCEPT_DISCONNECT) {
-							socket.shutdownInput();
-							out.println(mapper.writeValueAsString(output));
 							
+							out.println(mapper.writeValueAsString(output));
 							closeThread();
-							break outerwhile;
 							
 						} else if (output.getMessageType() == Message.ACCEPT_ID_REQUEST) {
 							connectedClientId = output.getClientId();
@@ -68,7 +65,7 @@ public class ServerThread extends Thread {
 					}
 
 				}
-				if (socket.getInputStream().read() == -1) {
+				if (!socket.isClosed() && inputLine == null) {
 					logger.warn("Connection closed abbruptly");
 					closeThread();
 					logger.warn("Socket was terminated successfully");
@@ -76,14 +73,7 @@ public class ServerThread extends Thread {
 				}
 
 			} catch (IOException e1) {
-				logger.error(connectedClientId + " Could not connect to socket: ", e1.getLocalizedMessage());
 				logger.error(e1.getMessage());
-				try {
-					sleep(5000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 			}
 		}
 
@@ -103,7 +93,7 @@ public class ServerThread extends Thread {
 	}
 
 	private void closeThread() throws IOException {
-		//socket.shutdownInput();
+		socket.shutdownInput();
 		int index = -1;
 		for (int i = 0; i < App.mockClientList.size(); i++) {
 			MockClient cli = App.mockClientList.get(i);
@@ -118,6 +108,8 @@ public class ServerThread extends Thread {
 		}
 		socket.shutdownOutput();
 		socket.close();
+		
+		this.interrupt();
 		
 	}
 
